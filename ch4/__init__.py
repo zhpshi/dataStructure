b13ee981b3f0920f017b617e527c837b3ac7dc9a
zpshi <zhaopshi@gmail.com>
2022-02-11 15:04:32 +0800

    ch4 update

---
+++ ch4/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 28/1/2022 7:42 PM
+@Author  : Zhaopeng SHI
+@Email   : zhaopshi@gmail.com
+@File    : __init__.py.py
+"""
+
+
+# import package here
+
+def main():
+    return 0
+
+
+if __name__ == '__main__':
+    main()
---
+++ ch4/convertor.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+@Time    : 28/1/2022 7:44 PM
+@Author  : Zhaopeng SHI
+@Email   : zhaopshi@gmail.com
+@File    : convertor.py
+"""
+
+
+# import package here
+def toStr(n, base):
+    converString = "0123456789ABCDEF"
+    if n < base:
+        return converString[n]
+    else:
+        return toStr(n // base, base) + converString[n % base]
+def main():
+    print(toStr(345, 2))
+    return 0
+
+
+if __name__ == '__main__':
+    main()
